e849bccaa3f29565beb05690401b5c6a497d2208
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2018-12-29 17:52:59 -0500

    refactor: remove Api.java

--- app/src/main/java/com/example/ioutd/bakingapp/utilities/Api.java
+++
@@ -1,9 +0,0 @@
-package com.example.ioutd.bakingapp.utilities;
-
-/**
- * Created by ioutd on 1/16/2018.
- */
-
-class Api {
-    public static final String API_KEY = "AIzaSyCOAwE2BP3VCmuyhMloYaxh7EH4ZUpzghc";
-}

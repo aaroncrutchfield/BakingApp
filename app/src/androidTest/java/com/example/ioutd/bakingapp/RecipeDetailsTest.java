ddffbdbd970a660a40e337deb54673c4761191d4
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2018-12-30 11:25:51 -0500

    fix: Launch RecipeDetailsActivity test with Extras

--- app/src/androidTest/java/com/example/ioutd/bakingapp/RecipeDetailsTest.java
+++ app/src/androidTest/java/com/example/ioutd/bakingapp/RecipeDetailsTest.java
@@ -1,10 +1,12 @@
 package com.example.ioutd.bakingapp;
 
 import android.content.Intent;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.Fragment;
 
 import com.example.ioutd.bakingapp.ui.RecipeDetailsActivity;
+import com.example.ioutd.bakingapp.ui.RecipeDetailsFragment;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -19,34 +21,55 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class RecipeDetailsTest {
 
-    public static final String RECIPE_NAME = "recipeName";
-    public static final String RECIPE_ID = "recipeID";
-    public static final String STEP_ID = "stepID";
+    private static final String RECIPE_NAME = "recipeName";
+    private static final String RECIPE_ID = "recipeID";
+    private static final String STEP_ID = "stepID";
+    private static final String CONTENT_POSITION = "contentPosition";
 
     private String recipeName = "Nutella Pie";
     private int recipeID = 1;
     private int introStepID = 100;
+    private long contentPosition = 0;
 
     @Rule
-    public IntentsTestRule<RecipeDetailsActivity> mIntentsTestRule = new IntentsTestRule<>(RecipeDetailsActivity.class);
+    public ActivityTestRule<RecipeDetailsActivity> mIntentsTestRule =
+            new ActivityTestRule<RecipeDetailsActivity>(RecipeDetailsActivity.class){
+                @Override
+                protected Intent getActivityIntent() {
+                    Intent intent = new Intent();
+                    intent.putExtra(RECIPE_ID, recipeID);
+                    intent.putExtra(RECIPE_NAME, recipeName);
+                    intent.putExtra(STEP_ID, introStepID);
+                    intent.putExtra(CONTENT_POSITION, contentPosition);
+
+                    return intent;
+                }
+            };
+
 
     @Before
-    public void setupIncomingIntent() {
-        Intent intent = new Intent();
-        intent.putExtra(RECIPE_ID, recipeID);
-        intent.putExtra(RECIPE_NAME, recipeName);
-        intent.putExtra(STEP_ID, introStepID);
+    public void init() {
+        Fragment fragment = RecipeDetailsFragment.newInstance(recipeID, recipeName);
 
-        // java.lang.RuntimeException: Could not launch intent
-        // is using a Fragment causing the issue?
-        // TODO: 12/29/18 how to put extras inside of intent before launching the activity
-        mIntentsTestRule.launchActivity(intent);
+        mIntentsTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction()
+                .replace(R.id.recipe_details_container, fragment)
+                .commit();
     }
 
     @Test
-    public void vanillaIngredientDisplayed() {
+    public void checkFirstIngredient() {
+        onView(withText("5.0 TBLSP"))
+                .check(matches(isDisplayed()));
+
         onView(withText("vanilla"))
                 .check(matches(isDisplayed()));
 
+    }
+
+    @Test
+    public void checkFirstStep() {
+        onView(withText("Recipe Introduction"))
+                .check(matches(isDisplayed()));
     }
 }
